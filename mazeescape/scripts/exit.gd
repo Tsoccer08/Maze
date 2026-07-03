a0ade3768a586fd3baf71b3941dd88d78d712618
Tsoccer08 <tjustesen08@gmail.com>
2026-07-03 08:13:06 -0600

    Add files via upload

---
+++ mazeescape/scripts/exit.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var next_level: String = ""
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.name == "Player" and next_level != "":
+		get_tree().change_scene_to_file(next_level)
